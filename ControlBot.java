f3a0cdb81608464667e4d96d46973423ed045efe
limj <julienlim94@gmail.com>
2023-01-07 00:02:13 +0100

    Update

--- ControlBot.java
+++
@@ -1,9 +0,0 @@
-public class ControlBot {
-    VueMain main;
-    Bot b;
-
-    public ControlBot(VueMain main, Bot b) {
-        this.main=main;
-        this.b=b;
-    }
-}
--- Joueur.java
+++
@@ -1,5 +0,0 @@
-public class Joueur extends AbstractJoueur {
-    public Joueur(int point, int numeroDeJoueur) {
-        super(point, numeroDeJoueur);
-    }
-}
